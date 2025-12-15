d7a0a29a76ce47b369117997e21c6fe7136af052
Mobahiro <hammerswing12@gmail.com>
2025-12-15 14:07:14 +0800

    fix migration error

--- config/settings/base.py
+++ config/settings/base.py
@@ -20,7 +20,6 @@ DEBUG = False
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
